7367b100bc992f95d5dcf2d5257898faf0e9f86a
umekow <umekolashun@gmail.com>
2020-02-13 22:52:49 -0600

    created player and room

--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player(): 
+    def __init__(self, room, name): 
+        room = self.room
+        name = self.name
+
+    def 
