50f4634db8c488c49eb41eaacf927efc4e543b43
umekow <umekolashun@gmail.com>
2020-02-21 14:19:05 -0600

    room

--- src/room.py
+++ src/room.py
@@ -1,9 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
-
 class Room(): 
     
     def __init__(self, name, description): 
         name = self.name
         description = self.description
+
+    def get_name(self): 
+        return self.name 
+    
+    def get_description(self): 
+        return self.get_description
+
