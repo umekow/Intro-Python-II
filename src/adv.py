de8964517ece1319c14a6a38cdab5c3b15b16c35
Restyled.io <commits@restyled.io>
2020-03-11 22:59:48 +0000

    Restyled by yapf

--- src/adv.py
+++ src/adv.py
@@ -3,35 +3,38 @@ from room import Room
 
 # Purpose: Allows player to move between rooms
 
-
 # Declare all the rooms
 
 room = {
-    "outside": Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
-    "foyer": Room(
+    "outside":
+    Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    "foyer":
+    Room(
         "Foyer",
         """Dim light filters in from the south. Dusty
 passages run north and east.""",
     ),
-    "overlook": Room(
+    "overlook":
+    Room(
         "Grand Overlook",
         """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm.""",
     ),
-    "narrow": Room(
+    "narrow":
+    Room(
         "Narrow Passage",
         """The narrow passage bends here from west
 to north. The smell of gold permeates the air.""",
     ),
-    "treasure": Room(
+    "treasure":
+    Room(
         "Treasure Chamber",
         """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""",
     ),
 }
-
 
 # Link rooms together
 
@@ -45,7 +48,6 @@ username = input("Please enter your username: ")
 # Make a new player object that is currently in the 'outside' room.
 
 user_player = Player(username, room["outside"])
-
 
 # Write a loop that:
 #
