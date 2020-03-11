c607a54ecdc1f3fa6da931703482bb0cab3d619e
umekow <umekolashun@gmail.com>
2020-03-11 18:07:55 -0500

    added attributes to room class

--- src/room.py
+++ src/room.py
@@ -9,6 +9,10 @@ class Room:
     def __init__(self, name, description, n_to, s_to, w_to, e_to): 
         self.name = name 
         self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.w_to = w_to
+        self.e_to = e_to
 
 
 
